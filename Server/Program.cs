62c3fcfef8e1794e41f2d4125598b05f303742fc
Sasha Dragan <dragan.alexandr.902@gmail.com>
2023-10-24 12:30:30 +0500

    3

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddSingleton<MqttClient>();
 var app = builder.Build();
 
 
